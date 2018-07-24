067654bc1bcda564547c61f83f06aa69c5654bef
Guillermo Capittini <gcapittini@hotmail.com>
2018-07-24 13:41:40 -0300

    Partial funct

---
+++ 15_tests_subseq/test-subseq.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+size_t maxSeq(int * array, size_t n);
+
+int main() {
+    int array1[] = {1, 1, 1, 1}
+    if (maxSeq(array1[], 4) != 1){
+        return EXIT_FAILURE
+    }
+    return EXIT_SUCCESS
+}
