ffcf51ef91fd99e44991ce38f59e25bbe5b4e86a
Guillermo Capittini <gcapittini@hotmail.com>
2018-07-24 12:43:39 -0300

    Did Assignment 14

--- 14_array_max/arrayMax.c
+++ 14_array_max/arrayMax.c
@@ -2,7 +2,19 @@
 #include <stdlib.h>
 
 int * arrayMax(int * array, int n) {
-  return NULL;
+  if (n == 0){
+    return NULL;
+  }
+  int localMax = *array;
+  int * maxPointer = array;
+  for (int i = 1; i < n; i++) {
+      array += 1;
+      if (*array > localMax) {
+          localMax = *array;
+          maxPointer = array;
+      }
+  }
+  return maxPointer;
 }
 
 void doTest(int * array, int n) {
