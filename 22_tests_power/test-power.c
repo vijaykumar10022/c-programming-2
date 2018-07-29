e96742d7718bde6d951c720d5fb69f68c3088b84
Coursera Learner <nobody@nowhere.nul>
2018-07-29 23:34:27 +0000

    Did Assignment 22

--- 22_tests_power/test-power.c
+++ 22_tests_power/test-power.c
@@ -1,11 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
 unsigned power(unsigned x, unsigned y);
 
 int main() {
     if (power(4, 4) != 256) {
         return EXIT_FAILURE;
     }
-    if (power(-2, 1) != -2) {
-        return EXIT_FAILURE;
+//    if (power(-2, 1) != -2) {
+//        return EXIT_FAILURE;
+//    }
+    if (power(0, 1) != 0) {
+	return EXIT_FAILURE;
     }
+    if (power(30, 1) != 30) {
+	return EXIT_FAILURE;
+    }
+    if (power(4, 0) != 1) {
+	return EXIT_FAILURE;
+    }
+    if (power(256, 2) != 65536) {
+	return EXIT_FAILURE;
+    }
+    if (power(0, 0) != 1) {
+	return EXIT_FAILURE;
+    }
+
     return EXIT_SUCCESS;
 }
